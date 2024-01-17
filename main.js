9ff9fa500cade3c3e59a53931db27b27979ce6c2
almelo <akaalga@proton.me>
2024-01-17 17:36:21 -0300

    feat: countdown, game over screen and hit sound

--- main.js
+++ main.js
@@ -3,48 +3,75 @@ const state = {
 		tiles: document.querySelectorAll(".tile"),
 		ralph: document.querySelector(".ralph"),
 		time: document.querySelector("#time"),
-		score: document.querySelector("#score")
+		score: document.querySelector("#score"),
+		menu: document.querySelector(".menu")
 	},
 	value: {
-		intervalId: null,
-		gamePace: 1000,
-		ralphTile: 0,
+		ralphTileId: -1,
 		score: 0,
+		time: 30,
+	},
+	action: {
+		ralphPaceId: setInterval(randomRalphSpawn, 1000),
+		countdownId: setInterval(countdown, 1000)
 	}
 }
 
 function randomRalphSpawn() {
 	const tiles = state.view.tiles;
 
+	// clear tiles state
 	tiles.forEach(
 		(tile) => {
 			tile.classList.remove("ralph");
 		}
 	);
 
+	// draw a new ralph's tile
 	const tileIdx = Math.floor(Math.random() * 9);
+
 	tiles[tileIdx].classList.add("ralph");
-	state.value.ralphTile = tiles[tileIdx].id;
+	state.value.ralphTileId = tiles[tileIdx].id;
 }
 
 function listenTileClicked() {
 	state.view.tiles.forEach(
 		(tile) => {
 			tile.addEventListener("mousedown", () => {
-				if (tile.id == state.value.ralphTile) {
+				if (tile.id == state.value.ralphTileId &&
+					state.value.time > 0) {
+					playSound("hit");
 					state.value.score++;
 					state.view.score.textContent = state.value.score;
-					state.value.ralphTile = 0;
+					state.value.ralphTileId = -1;
 				}
 			});
-		}	
+		}
 	);
 }
 
-function main() {
-	let intervalId = state.value.intervalId;
+function countdown() {
+	state.view.time.textContent = state.value.time;
 
-	intervalId = setInterval(randomRalphSpawn, state.value.gamePace);
+	if (state.value.time <= 0) {
+		clearInterval(state.action.ralphPaceId);
+		clearInterval(state.action.countdownId);
+
+		state.view.menu.innerHTML = `
+			<h2>You scored ${state.value.score} points!<h2>
+			<h2>Press F5 to try again.<h2>`
+	}
+	state.value.time--;
+}
+
+function playSound(soundName) {
+	let audio = new Audio(`audios/${soundName}.m4a`);
+
+	audio.volume = 0.1;
+	audio.play();
+}
+
+function main() {
 	listenTileClicked();
 }
 
